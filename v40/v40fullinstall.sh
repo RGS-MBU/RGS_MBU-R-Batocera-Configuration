9dbe5f85dd8821a68b5b53aacbf90acd4d38ef9e
Alexandre Derumier <aderumier@gmail.com>
2024-09-24 12:52:04 +0200

    add v40 fullinstall script

---
+++ v40/v40fullinstall.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ARCHIVE="rgs-v40-full.tar.gz"
+ARCHIVEPATH="/userdata/$ARCHIVE"
+
+#download pack
+echo "Downloading new files..."
+wget --progress=dot:binary --no-check-certificate --no-cache --no-cookies -O $ARCHIVEPATH "http://rgsretro1986.ds78102.seedhost.eu/update/v40/rgs-v40-full.tar.gz"
+
+
+echo 'Deleting old directories'
+rm -rf /userdata/bios
+rm -rf /userdata/cheats
+rm -rf /userdata/extractions
+rm -rf /userdata/decorations
+rm -rf /userdata/kodi
+rm -rf /userdata/library
+rm -rf /userdata/music
+rm -rf /userdata/recordings
+rm -rf /userdata/saves
+rm -rf /userdata/screenshots
+rm -rf /userdata/shaders
+rm -rf /userdata/splash
+rm -rf /userdata/system
+rm -rf /userdata/themes
+
+echo "uncompress archive..."
+
+tar -xvzf $ARCHIVEPATH --directory /userdata/
+rm $ARCHIVEPATH
+
+echo 'Upgrade finished. reboot.'
+sleep 2
+shutdown -r now
