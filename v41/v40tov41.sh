e20c47b4509fecde3015bb2052db6301c1b57983
Alexandre Derumier <aderumier@gmail.com>
2025-01-03 17:15:13 +0100

    fix v40 to v41 upgrade script

--- v41/v40tov41.sh
+++ v41/v40tov41.sh
@@ -42,29 +42,29 @@ rm /userdata/system/configs/gaelco/launcher.py.save
 rm /userdata/system/configs/sudachi
 rm /userdata/system/configs/winebat/GeneratorImporter.py
 rm /userdata/system/configs/winebat/launcher.py
-rm /userdata/themes/Carbon-PixN/art/logos/atari.svg
-rm /userdata/themes/Hypermax-Plus-PixN/Gamelist-text anim.xml
-rm /userdata/themes/Hypermax-Plus-PixN/_inc/logos/CloneHero.jpg
-rm /userdata/themes/Hypermax-Plus-PixN/_inc/logos/bk.rar
-rm /userdata/themes/Hypermax-Plus-PixN/_inc/logos/electronika.png
-rm /userdata/themes/Hypermax-Plus-PixN/_inc/music/Beach (8-Bit Remix) - New Super Mario Bros.ogg
-rm /userdata/themes/Hypermax-Plus-PixN/_inc/music/Fracktail Battle (8-Bit Cover) - Super Paper Mario.ogg
-rm /userdata/themes/Hypermax-Plus-PixN/_inc/music/Francis Battle 8Bit Remix  Super Paper Mario BT.ogg
-rm /userdata/themes/Hypermax-Plus-PixN/_inc/music/Mars Attacks Main Theme.mp3
-rm /userdata/themes/Hypermax-Plus-PixN/_inc/music/Mars Attacks Main Theme.wav
-rm /userdata/themes/Hypermax-Plus-PixN/_inc/music/New Super Mario Bros.ogg
-rm /userdata/themes/Hypermax-Plus-PixN/_inc/music/Red Streamer Battle (8-Bit Remix) - Paper Mario_ The Origami King.ogg
-rm /userdata/themes/Hypermax-Plus-PixN/_inc/music/Vellumental Battle 8Bit Remix  Paper Mario The Origami King.ogg
-rm /userdata/themes/Hypermax-Plus-PixN/_inc/scotty.mp4
+rm '/userdata/themes/Carbon-PixN/art/logos/atari.svg'
+rm '/userdata/themes/Hypermax-Plus-PixN/Gamelist-text anim.xml'
+rm '/userdata/themes/Hypermax-Plus-PixN/_inc/logos/CloneHero.jpg'
+rm '/userdata/themes/Hypermax-Plus-PixN/_inc/logos/bk.rar'
+rm '/userdata/themes/Hypermax-Plus-PixN/_inc/logos/electronika.png'
+rm '/userdata/themes/Hypermax-Plus-PixN/_inc/music/Beach (8-Bit Remix) - New Super Mario Bros.ogg'
+rm '/userdata/themes/Hypermax-Plus-PixN/_inc/music/Fracktail Battle (8-Bit Cover) - Super Paper Mario.ogg'
+rm '/userdata/themes/Hypermax-Plus-PixN/_inc/music/Francis Battle 8Bit Remix  Super Paper Mario BT.ogg'
+rm '/userdata/themes/Hypermax-Plus-PixN/_inc/music/Mars Attacks Main Theme.mp3'
+rm '/userdata/themes/Hypermax-Plus-PixN/_inc/music/Mars Attacks Main Theme.wav'
+rm '/userdata/themes/Hypermax-Plus-PixN/_inc/music/New Super Mario Bros.ogg'
+rm '/userdata/themes/Hypermax-Plus-PixN/_inc/music/Red Streamer Battle (8-Bit Remix) - Paper Mario_ The Origami King.ogg'
+rm '/userdata/themes/Hypermax-Plus-PixN/_inc/music/Vellumental Battle 8Bit Remix  Paper Mario The Origami King.ogg'
+rm '/userdata/themes/Hypermax-Plus-PixN/_inc/scotty.mp4'
 rm -rf themes/Hypermax-Plus-PixN/_inc/text anim/*
-rm /userdata/themes/Hypermax-Plus-PixN/_inc/wallpaper1/D0IIRGVUcAEcS5K.png
-rm /userdata/themes/Hypermax-Plus-PixN/_inc/wallpaper1/NINTCHDBPICT000536074229-e1572807808559.webp
-rm /userdata/themes/Hypermax-Plus-PixN/_inc/wallpaper1/desktop.ini
-rm /userdata/themes/Hypermax-Plus-PixN/_inc/wallpaper1/neshd.png
-rm /userdata/themes/Hypermax-Plus-PixN/_template/4815203235791361221411321768176177154196199443.ico
-rm /userdata/themes/Hypermax-Plus-PixN/capcom/desktop.ini
-rm /userdata/themes/Hypermax-Plus-PixN/default/desktop.ini
-rm /userdata/themes/Hypermax-Plus-PixN/konami/desktop.ini
+rm '/userdata/themes/Hypermax-Plus-PixN/_inc/wallpaper1/D0IIRGVUcAEcS5K.png'
+rm '/userdata/themes/Hypermax-Plus-PixN/_inc/wallpaper1/NINTCHDBPICT000536074229-e1572807808559.webp'
+rm '/userdata/themes/Hypermax-Plus-PixN/_inc/wallpaper1/desktop.ini'
+rm '/userdata/themes/Hypermax-Plus-PixN/_inc/wallpaper1/neshd.png'
+rm '/userdata/themes/Hypermax-Plus-PixN/_template/4815203235791361221411321768176177154196199443.ico'
+rm '/userdata/themes/Hypermax-Plus-PixN/capcom/desktop.ini'
+rm '/userdata/themes/Hypermax-Plus-PixN/default/desktop.ini'
+rm '/userdata/themes/Hypermax-Plus-PixN/konami/desktop.ini'
 
 echo "uncompress archive..."
 
