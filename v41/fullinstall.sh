53580cb3495e02b5e659e5ca98e62cccd39db109
Alexandre Derumier <aderumier@gmail.com>
2025-01-12 17:17:22 +0100

    fix

--- v41/fullinstall.sh
+++ v41/fullinstall.sh
@@ -4,17 +4,34 @@ ARCHIVE="fullinstall.tar.gz"
 ARCHIVEPATH="/userdata/$ARCHIVE"
 
 #download rclone
-mkdir -p /userdata/system/.config/rclone/
-wget --progress=dot:binary --no-check-certificate --no-cache --no-cookies -O /usr/bin/rclone https://pixeldrain.com/api/filesystem/YD1k3dcE/rclone
-wget --progress=dot:binary --no-check-certificate --no-cache --no-cookies -O /userdata/system/.config/rclone/rclone.conf https://pixeldrain.com/api/filesystem/YD1k3dcE/rclone.conf
-chmod +x /usr/bin/rclone
+#mkdir -p /userdata/system/.config/rclone/
+#wget --progress=dot:binary --no-check-certificate --no-cache --no-cookies -O /usr/bin/rclone https://pixeldrain.com/api/filesystem/YD1k3dcE/rclone
+#wget --progress=dot:binary --no-check-certificate --no-cache --no-cookies -O /userdata/system/.config/rclone/rclone.conf https://pixeldrain.com/api/filesystem/YD1k3dcE/rclone.conf
+#chmod +x /usr/bin/rclone
+
 
 #download pack
 echo "Downloading new files..."
-rclone sync "PixN-Updates:/v41/$ARCHIVE" $ARCHIVEPATH --progress
+#rclone sync "PixN-Updates:/v41/$ARCHIVE" $ARCHIVEPATH --progress
+wget --progress=dot:binary --no-check-certificate --no-cache --no-cookies -O $ARCHIVEPATH https://pixeldrain.com/api/filesystem/YD1k3dcE/v41/fullinstall.tar.gz
+
+echo "uncompress archive..."
+sleep 5
+
+
+if tar -xvzf $ARCHIVEPATH --directory /userdata/
+then
+    echo 'uncompressing archive ok'
+else
+    echo 'error with uncompressing archive. We stop here.'
+    sleep 30
+    exit
+fi
+
 
 echo 'Deleting old directories'
 sleep 5
+
 
 rm -rf /userdata/bios
 rm -rf /userdata/cheats
@@ -31,10 +48,7 @@ rm -rf /userdata/splash
 rm -rf /userdata/system
 rm -rf /userdata/themes
 
-echo "uncompress archive..."
-sleep 5
 
-tar -xvzf $ARCHIVEPATH --directory /userdata/
 mv /userdata/Batocera/roms/rgs /userdata/roms/
 rm -rf /userdata/Batocera/roms
 mv /userdata/Batocera/* /userdata/
