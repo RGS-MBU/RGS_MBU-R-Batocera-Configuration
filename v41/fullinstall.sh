9998c83df201e4de593eef1eaed7cc0cd5d1391f
SPiRiT <batospirit@gmail.com>
2025-05-12 14:21:41 +0200

    update full install script to not delete parent directory

--- v41/fullinstall.sh
+++ v41/fullinstall.sh
@@ -39,25 +39,40 @@ echo 'Deleting old directories'
 sleep 5
 
 
-rm -rf /userdata/bios
-rm -rf /userdata/cheats
-rm -rf /userdata/extractions
-rm -rf /userdata/decorations
-rm -rf /userdata/kodi
-rm -rf /userdata/library
-rm -rf /userdata/music
-rm -rf /userdata/recordings
-rm -rf /userdata/saves
-rm -rf /userdata/screenshots
-rm -rf /userdata/shaders
-rm -rf /userdata/splash
-rm -rf /userdata/system
-rm -rf /userdata/themes
+rm -rf /userdata/bios/*
+rm -rf /userdata/cheats/*
+rm -rf /userdata/extractions/*
+rm -rf /userdata/decorations/*
+rm -rf /userdata/kodi/*
+rm -rf /userdata/library/*
+rm -rf /userdata/music/*
+rm -rf /userdata/recordings/*
+rm -rf /userdata/saves/*
+rm -rf /userdata/screenshots/*
+rm -rf /userdata/shaders/*
+rm -rf /userdata/splash/*
+rm -rf /userdata/system/*
+rm -rf /userdata/themes/*
 
 
 mv /userdata/Batocera/roms/rgs /userdata/roms/
 rm -rf /userdata/Batocera/roms
-mv /userdata/Batocera/* /userdata/
+
+mv /userdata/Batocera/bios/* /userdata/bios/
+mv /userdata/Batocera/cheats/* /userdata/cheats/
+mv /userdata/Batocera/extractions/* /userdata/extractions/
+mv /userdata/Batocera/decorations/* /userdata/decorations/
+mv /userdata/Batocera/kodi/* /userdata/kodi/
+mv /userdata/Batocera/library/* /userdata/library/
+mv /userdata/Batocera/music/* /userdata/music/
+mv /userdata/Batocera/recordings/* /userdata/recordings/
+mv /userdata/Batocera/saves/* /userdata/saves/
+mv /userdata/Batocera/screenshots/* /userdata/screenshots/
+mv /userdata/Batocera/shaders/* /userdata/shaders/
+mv /userdata/Batocera/splash/* /userdata/splash/
+mv /userdata/Batocera/system/* /userdata/system/
+mv /userdata/Batocera/themes/* /userdata/themes/
+
 rm -rf /userdata/Batocera
 
 
