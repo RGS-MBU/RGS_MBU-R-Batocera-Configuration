c5021c47a7116335aca778d36deea789c6305163
SPiRiT <batospirit@gmail.com>
2025-10-15 17:25:26 +0200

    42.1

---
+++ create_upgrade_v41_to_v42script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+RELEASE="v41"
+CURRENTCOMMIT="HEAD"
+PACKAGE="../RGS_MBU-R-Batocera-Configuration/v42/v41tov42.tar.gz"
+UPDATESCRIPT="v42/rgs_upgrade"
+
+cd ../Batocera
+
+#archive new && changed files
+rm $PACKAGE
+git diff --diff-filter=ACMRTUXB --name-only $RELEASE HEAD | sed 's/^/.\//' |tar -czf $PACKAGE -T -
+RENAMEDFILES=$(git diff $RELEASE HEAD|grep "rename from"|sed 's/rename from /rm \/userdata\//')
+RMFILES=$(git diff $RELEASE HEAD --diff-filter=D --summary|sed 's/delete mode 100644/rm/')
+
+
+cp system/rgs.version ../RGS_MBU-R-Batocera-Configuration/v42/version.txt
+
+cd ../RGS_MBU-R-Batocera-Configuration
+
+#create remove of  old files (deleted of renamed)
+cat rgs_upgrade_part1.template > $UPDATESCRIPT
+echo "$RENAMEDFILES" >> $UPDATESCRIPT
+echo "$RMFILES" >> $UPDATESCRIPT
+cat rgs_upgrade_part2.template >> $UPDATESCRIPT
+
+
