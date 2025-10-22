c251f92bc70683ea2f7bae26a872693cf27bd3d5
SPiRiT <batospirit@gmail.com>
2025-10-22 13:10:19 +0200

    fix

--- v42/fullinstall.sh
+++ v42/fullinstall.sh
@@ -284,13 +284,13 @@ if [ -d "/userdata/themes/ckau-book-PixN" ]; then
 fi
 
 
-read -p "Do you want to install theme Hypermax-Plus-PixN (y/n): " answer < /dev/tty
-if [[ "$answer" =~ ^[Yy]$ ]]; then
+read -p "Do you want to install theme Hypermax-Plus-PixN (y/n): " answer1 < /dev/tty
+if [[ "$answer1" =~ ^[Yy]$ ]]; then
     mkdir -p /userdata/themes/Hypermax-Plus-PixN
 fi
 
-read -p "Do you want to install theme Carbon-PixN (y/n): " answer < /dev/tty
-if [[ "$answer" =~ ^[Yy]$ ]]; then
+read -p "Do you want to install theme Carbon-PixN (y/n): " answer2 < /dev/tty
+if [[ "$answer2" =~ ^[Yy]$ ]]; then
     mkdir -p /userdata/themes/Carbon-PixN
 fi
 
